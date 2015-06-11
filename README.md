fdacd2f4f2f261c3ed00a36d5b6fd3c0f83911c1
Philippe Desjardins-Proulx <philippe.d.proulx@gmail.com>
2015-06-11 19:55:44 -0400

    install script

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ Alchemy 2
 =========
 A copy of the [official SVN repository](http://code.google.com/p/alchemy-2/).
 
+Installation
+------------
+Alchemy was developed/tested on Linux (Fedora Core 5)
+
+License
+-------
+MIT
+
---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install dependencies
+sudo apt-get install g++-4.4 flex
+
+# Make the folder to put the bin
+makedir -p bin/obj
+
+# Download & install Bison 2
+wget http://ftp.gnu.org/gnu/bison/bison-2.0.tar.gz
+tar -xvf bison-2.0.tar.gz
+cd bison-2.0
+./configure
+make
+sudo make install
+cd ..
+
+# Compile Alchemy
+cd src
+make depend
+make
+
